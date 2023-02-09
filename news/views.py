8ce0db1c2a065f34b424cd43c8f6f5cdcab91a26
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-09 02:23:52 -0500

    Configure DetailView, add post template

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,3 +8,9 @@ class PostsList(ListView):
     ordering = 'title'
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
