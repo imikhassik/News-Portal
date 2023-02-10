765a3b7734985a2026eb138617d94549eecd814e
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-10 04:40:15 -0500

    Add censor filter, update post, posts templates

---
+++ news/templatetags/custom_filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+
+register = template.Library()
+
+
+UNDESIRABLE_WORDS = [
+    'последний',
+]
+
+
+@register.filter()
+def censor(text):
+    censored_text = []
+    for word in text.split():
+        if word in UNDESIRABLE_WORDS:
+            censored_text.append(word[0]+''.join(['*' for _ in word]))
+        else:
+            censored_text.append(word)
+    return ' '.join(censored_text)
