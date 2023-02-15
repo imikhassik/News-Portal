fe67f557c746e66d853f5e37f77ab08cd7bbbbf9
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-15 03:50:17 -0500

    Fix total num of news in paginated view

--- news/views.py
+++ news/views.py
@@ -7,7 +7,12 @@ class PostsList(ListView):
     ordering = '-created_on'
     template_name = 'posts.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_of_news'] = len(Post.objects.all())
+        return context
 
 
 class PostDetail(DetailView):
