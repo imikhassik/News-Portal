ca3e0a2c16a550fba9020d27b40b9d5b22070877
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-16 09:43:53 -0500

    Update filter with DateInput widget

--- news/filters.py
+++ news/filters.py
@@ -1,12 +1,18 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, DateTimeFilter
+from django import forms
+
 from .models import Post
 
 
 class PostsFilter(FilterSet):
+    created_on = DateTimeFilter(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        lookup_expr='date__gt',
+    )
+
     class Meta:
         model = Post
         fields = {
             'title': ['icontains'],
             'author__user': ['exact'],
-            'created_on': ['gt']
         }
