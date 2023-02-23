368489cc4608d8510f57d50e0a5e4288767828b3
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-23 02:54:16 -0500

    Add account/base.html, update urls

--- NewsPaper/urls.py
+++ NewsPaper/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('posts/', include('news.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
