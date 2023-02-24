ee111163ee1d3ccbe610b062fbfe47ffbef5eb57
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-24 02:29:39 -0500

    Add automatic group setting at signup

---
+++ accounts/forms.py
@@ -0,0 +1,11 @@
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
+
+class CommonSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(CommonSignupForm, self).save(request)
+        common_group = Group.objects.get(name='common')
+        common_group.user_set.add(user)
+        return user
