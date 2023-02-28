b38de89fbbdc91cb4ba7483c08357775dea8637c
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-28 02:38:44 -0500

    Add CategorySubscriber relation

--- news/models.py
+++ news/models.py
@@ -27,6 +27,7 @@ class Author(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    subscribers = models.ManyToManyField(User, through='CategorySubscriber')
 
     def __str__(self):
         return f"{self.name}"
@@ -64,6 +65,11 @@ class Post(models.Model):
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class CategorySubscriber(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
