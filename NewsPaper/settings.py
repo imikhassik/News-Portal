2267a12dd3470cc6ca05b68a9bb6e4a436157871
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-06 03:55:04 -0500

    Add apscheduler

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     
     'news.apps.NewsConfig',
     'accounts',
+
+    'django_apscheduler',
 ]
 
 LOGIN_URL = '/accounts/login/'
@@ -167,3 +169,6 @@ EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
 EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = str(os.getenv('EMAIL_HOST_USER')) + '@yandex.ru'
+
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
+APSCHEDULER_RUN_NOW_TIMEOUT = 25
---
+++ news/management/commands/runapscheduler.py
@@ -0,0 +1,59 @@
+import logging
+
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+logger = logging.getLogger(__name__)
+
+
+def my_job():
+    #  Your job processing logic here...
+    print('hello from job')
+
+
+def delete_old_job_executions(max_age=604_800):
+    """This job deletes all apscheduler job executions older than `max_age` from the database."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs apscheduler."
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            my_job,
+            trigger=CronTrigger(second="*/10"),
+            id="my_job",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'my_job'.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info(
+            "Added weekly job: 'delete_old_job_executions'."
+        )
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
