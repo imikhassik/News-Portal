ed6f0f48540c74904a2cc1a4e807cb0cc8c6fcf9
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-06 03:41:49 -0500

    Add link to post in email template

--- news/signals.py
+++ news/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
 
 from .models import Post
 
@@ -17,15 +18,17 @@ def notify_subscribers(sender, instance, action, **kwargs):
                 'post_email.html',
                 {
                     'post': instance,
-                    'username': subscriber.get("subscribers__username")
+                    'username': subscriber.get("subscribers__username"),
+                    'domain': Site.objects.get_current().domain,
+                    'post_url': instance.get_absolute_url(),
                 }
             )
 
             msg = EmailMultiAlternatives(
-                subject=instance.title,
-                body=instance.text,
-                from_email='ilya.mikhassik@yandex.ru',
-                to=[subscriber.get("subscribers__email")]
-            )
+            subject=instance.title,
+            body=instance.text,
+            from_email='ilya.mikhassik@yandex.ru',
+            to=[subscriber.get("subscribers__email")],
+        )
             msg.attach_alternative(html_content, "text/html")
             msg.send()
