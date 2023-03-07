29a3a5cd7318eda1c5dbeb3a18a42fed8be7dccd
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-07 02:00:08 -0500

    Separate jobs from scheduler, change job trigger to weekly

--- news/management/commands/runapscheduler.py
+++ news/management/commands/runapscheduler.py
@@ -1,12 +1,6 @@
 import logging
 
 from django.conf import settings
-from django.utils.timezone import datetime
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.contrib.sites.models import Site
-
-from datetime import timedelta
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -14,37 +8,10 @@ from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-from news.models import Post, Category
+from news.management.jobs.jobs import weekly_email_job
 
 logger = logging.getLogger(__name__)
 
-
-def my_job():
-    start_date = datetime.today() - timedelta(days=6)
-    this_weeks_posts = Post.objects.filter(created_on__gt=start_date)
-    for cat in Category.objects.all():
-        post_list = this_weeks_posts.filter(categories=cat)
-        if post_list:
-            subscribers = cat.subscribers.values('username', 'email')
-            recipients = []
-            for sub in subscribers:
-                recipients.append(sub['email'])
-
-            html_content = render_to_string(
-                'weekly_post_email.html', {
-                    'post_list': post_list.values('pk', 'title'),
-                    'domain': Site.objects.get_current().domain,
-                }
-            )
-
-            msg = EmailMultiAlternatives(
-                subject=f'{cat.name}: Посты за прошедшую неделю',
-                body="post_list",
-                from_email='ilya.mikhassik@yandex.ru',
-                to=recipients
-            )
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
 
 def delete_old_job_executions(max_age=604_800):
     """This job deletes all apscheduler job executions older than `max_age` from the database."""
@@ -59,13 +26,13 @@ class Command(BaseCommand):
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
         scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(second="*/10"),
-            id="my_job",
+            weekly_email_job,
+            trigger=CronTrigger(week="*"),
+            id="weekly_email_job",
             max_instances=1,
             replace_existing=True,
         )
-        logger.info("Added job 'my_job'.")
+        logger.info("Added job 'weekly_email_job'.")
 
         scheduler.add_job(
             delete_old_job_executions,
