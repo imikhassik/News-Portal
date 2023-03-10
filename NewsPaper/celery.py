4fb27146bc36ea1d5915a780ba3b4063c2a42fde
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-10 03:38:09 -0500

    Configure celery

---
+++ NewsPaper/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
+
+app = Celery('NewsPaper')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
