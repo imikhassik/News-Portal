e1cdcc1c8c9ffe6eaaddef2bc9b3d09c2c15f586
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-11 08:15:23 -0500

    Apply caching to news_list view

--- news/urls.py
+++ news/urls.py
@@ -10,9 +10,9 @@ from .views import (
 
 urlpatterns = [
     path('', cache_page(60)(PostsList.as_view()), name='posts_list'),
-    path('<int:pk>', cache_page(60 * 5)(PostDetail.as_view()), name='post_detail'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('search/', PostsSearch.as_view(), name='posts_search'),
-    path('news/', NewsList.as_view(), name='news_list'),
+    path('news/', cache_page(60 * 5)(NewsList.as_view()), name='news_list'),
     path('news/create/', NewsCreate.as_view(), name='news_create'),
     path('news/<int:pk>/edit/', PostUpdate.as_view(), name='news_update'),
     path('news/<int:pk>/delete/', PostDelete.as_view(), name='news_delete'),
