d30676c347793a9e01a1d03f3f7c5d7b6f0ffda9
Ilya Mikhasik <imikhassik@gmail.com>
2023-04-02 20:34:54 +0300

    Add CategoryAdmin and PostAdmin

--- news/admin.py
+++ news/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Post, Category
 
 
-admin.site.register(Post)
-admin.site.register(Category)
+def update_rating(modeladmin, request, queryset):
+    queryset.update(rating=10)
+update_rating.short_description = 'Задать рейтинг 10'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('type', 'created_on', 'title', 'rating', 'get_categories')
+    list_filter = ('type',)
+    search_fields = ('title', 'categories__name',)
+    actions = [update_rating]
+
+    @staticmethod
+    def get_categories(obj):
+        return ', '.join(category.name for category in obj.categories.all())
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
